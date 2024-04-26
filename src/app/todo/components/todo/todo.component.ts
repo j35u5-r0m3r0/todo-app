d0e4c6b218d21dcae65998b87c18abad1efa8d86
Romero Clairac <jesus.romero-clairac@capgemini.com>
2024-04-26 19:59:50 +0200

    crear servicio tasksService

--- src/app/todo/components/todo/todo.component.ts
+++ src/app/todo/components/todo/todo.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { Task } from '../../interfaces/tasks.interface';
+import { TasksService } from '../../services/tasks.service';
 
 @Component({
   selector: 'todo-root',
@@ -34,6 +35,12 @@ export class TodoComponent {
       isCompleted: false
     },
   ];
+
+  // otra forma de injectar un servicio en un componente
+  //private tasksSrv = inject(TasksService);
+  constructor(
+    private taskSrv: TasksService
+  ){}
 
   public onDeleteTask(index: number): void {
     this.tasks.splice(index,1);
