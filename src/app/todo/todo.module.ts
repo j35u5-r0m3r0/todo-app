121b790b6e795b5d036b0042cb0ee7b6704068d6
Romero Clairac <jesus.romero-clairac@capgemini.com>
2024-04-26 17:09:53 +0200

    crear componente todo

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { FormsModule} from "@angular/forms"
+import { FormsModule} from "@angular/forms";
+import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
-    declarations: [],
-    imports: [
-        CommonModule,
-        FormsModule
-    ]
+    declarations: [
+    TodoComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    TodoComponent
+  ]
 })
 export class Todomodule { }
